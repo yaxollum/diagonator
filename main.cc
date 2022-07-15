833521e227cace66bdd73b22fe23c8fdf2402b5c
Peter Ye <yaxollum@gmail.com>
2022-07-15 11:56:01 -0400

    close display

--- main.cc
+++ main.cc
@@ -3,11 +3,11 @@
 
 int main() {
   Display *display = XOpenDisplay(nullptr);
-  if (display != nullptr) {
-    std::cout << "Display is: " << display << "\n";
-  } else {
+  if (display == nullptr) {
     std::cerr << "diagonator: Unable to open the display. Make sure that "
                  "the $DISPLAY environment variable is set correctly.\n";
     return 1;
   }
+  std::cout << "Display is: " << display << "\n";
+  XCloseDisplay(display);
 }
