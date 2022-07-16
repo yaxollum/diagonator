9b1a596180c8bedf079d8cfe37da0f2264ea4235
Peter Ye <yaxollum@gmail.com>
2022-07-16 17:33:11 -0400

    Use XDisplayName in error message

--- main.cc
+++ main.cc
@@ -4,8 +4,9 @@
 int main() {
   Display *display = XOpenDisplay(nullptr);
   if (display == nullptr) {
-    std::cerr << "diagonator: Unable to open the display. Make sure that "
-                 "the $DISPLAY environment variable is set correctly.\n";
+    std::cerr << "diagonator: Unable to open display '" << XDisplayName(nullptr)
+              << "'. Make sure that the $DISPLAY environment variable is set "
+                 "correctly.\n";
     return 1;
   }
   std::cout << "Display is: " << display << "\n";
