df5a7f7a02ea28eafa3a6fc24fc6f532beff3a5f
Peter Ye <yaxollum@gmail.com>
2022-07-23 20:05:32 -0400

    Some basic config loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::path::PathBuf;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DiagonatorConfig {
+    diagonator_path: String,
+    diagonator_args: Vec<String>,
 }
+
+impl Default for DiagonatorConfig {
+    fn default() -> Self {
+        let diagonator_path: String = match dirs::executable_dir() {
+            Some(mut path) => {
+                path.push("diagonator");
+                path.to_string_lossy().to_string()
+            }
+            None => String::new(),
+        };
+        Self {
+            diagonator_path,
+            diagonator_args: Vec::new(),
+        }
+    }
+}
+
+enum ServerError {
+    NoConfigDir,
+}
+
+fn make_default_config(config_file_path: &PathBuf) {
+    eprintln!(
+        "Creating default config file at {}",
+        config_file_path.display()
+    );
+    toml::to_string_pretty(&DiagonatorConfig::default());
+}
+
+fn load_config() -> Result<DiagonatorConfig, ServerError> {
+    let mut config_file_path = dirs::config_dir().ok_or(ServerError::NoConfigDir)?;
+    config_file_path.push("diagonator-server");
+    config_file_path.push("config.toml");
+    if !config_file_path.exists() {
+        make_default_config(&config_file_path);
+    }
+    let config_file = File::open(config_file_path);
+    Ok(DiagonatorConfig::default())
+}
+
+fn main() {}
