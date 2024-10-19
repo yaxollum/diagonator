d05bddf2e4b12edcb683bb07e43d5fccfd421be7
Peter Ye <yaxollum@gmail.com>
2024-10-19 14:48:12 -0400

    refactor: move prompt_dmenu_time to utils

--- clients/diagonator_clients/deactivate_dmenu.py
+++ clients/diagonator_clients/deactivate_dmenu.py
@@ -1,50 +1,18 @@
 #!/usr/bin/env python3
 
-import datetime
 import sqlite3
-import subprocess
 import sys
 
 import requests
 
-from .utils import ANALYTICS_FILE, SERVER_URL, get_datetime_pair
+from .utils import ANALYTICS_FILE, SERVER_URL, get_datetime_pair, prompt_dmenu_time
 
 if len(sys.argv) < 2:
     sys.exit("Please specify the deactivation duration in seconds.")
 
 duration = int(sys.argv[1])
 
-DMENU_CMD = ["dmenu"] + sys.argv[2:]
-
-
-def correct():
-    def round_up(t):
-        h = t.hour
-        if h < 4:
-            h += 24
-        m = t.minute
-        if m < 30:
-            m = 30
-        else:
-            h += 1
-            m = 0
-        return f"{h:02}:{m:02}"
-
-    answer = (
-        subprocess.run(
-            DMENU_CMD,
-            input="",
-            capture_output=True,
-        )
-        .stdout.decode()
-        .strip("\n")
-    )
-    t = datetime.datetime.now()
-    t2 = t - datetime.timedelta(minutes=1)
-    return answer in (round_up(t), round_up(t2))
-
-
-if correct():
+if prompt_dmenu_time(sys.argv[2:]):
     print(
         requests.post(
             SERVER_URL, json={"type": "Deactivate", "duration": duration}
--- clients/diagonator_clients/utils.py
+++ clients/diagonator_clients/utils.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import subprocess
 
 SERVER_URL = os.getenv("DIAGONATOR_SERVER_URL", "http://localhost:3000")
 ANALYTICS_FILE = os.getenv("DIAGONATOR_ANALYTICS_FILE")
@@ -11,3 +12,34 @@ def get_datetime_pair():
     now = datetime.datetime.now()
     midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
     return (now.strftime("%Y-%m-%d"), (now - midnight).seconds)
+
+
+def prompt_dmenu_time(dmenu_options: list[str]) -> bool:
+    """Creates a dmenu prompt asking for the current time, rounded to the next half hour.
+    Returns a bool indicating whether the user input the correct time.
+    """
+
+    def round_up(t):
+        h = t.hour
+        if h < 4:
+            h += 24
+        m = t.minute
+        if m < 30:
+            m = 30
+        else:
+            h += 1
+            m = 0
+        return f"{h:02}:{m:02}"
+
+    answer = (
+        subprocess.run(
+            ["dmenu"] + dmenu_options,
+            input="",
+            capture_output=True,
+        )
+        .stdout.decode()
+        .strip("\n")
+    )
+    t = datetime.datetime.now()
+    t2 = t - datetime.timedelta(minutes=1)
+    return answer in (round_up(t), round_up(t2))
